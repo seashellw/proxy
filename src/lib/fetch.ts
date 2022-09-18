3596a28137455617f5624195fe344a95eac6f686
WangZhengHai <15839366985@163.com>
2022-09-18 13:26:30 +0800

    任务：增加fetch注释

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,4 +1,11 @@
+/**
+ * fetch封装类，提供get、post方法，提供请求拦截器和响应拦截器，
+ * 支持定义公共基础路径，支持解构使用
+ */
 export class Fetcher {
+  /**
+   * 将请求参数拼接至url
+   */
   static spliceURLSearchParams = (url: string, params?: any) => {
     if (!params) return url;
     let query = new URLSearchParams(url.split("?")[1] || undefined);
@@ -17,11 +24,14 @@ export class Fetcher {
   };
 
   constructor(option?: {
+    // 请求前拦截器
     onRequest?: (request: {
       input: string | URL;
       init?: RequestInit;
     }) => typeof request | undefined;
+    // 响应前拦截器
     onResponse?: (response: Response) => typeof response | undefined;
+    // 请求基础路径
     base?: string;
   }) {
     let { onRequest, onResponse, base } = option || {};
@@ -35,10 +45,12 @@ export class Fetcher {
       if (onRequest) {
         req = onRequest({ input, init });
       }
+      // 若请求拦截器返回空值，则不予发送
       if (!req) return;
       ({ input, init } = req);
       const res: Response | undefined = await fetch(input, init);
       if (onResponse) {
+        // 若响应拦截器返回空值，则返回空值
         return onResponse(res);
       }
       if (!res.ok) {
@@ -67,9 +79,11 @@ export class Fetcher {
       if (!body) {
         _body = undefined;
       } else if (body instanceof FormData) {
+        // 如果是FormData类型
         _body = body;
         headers = { "Content-Type": "multipart/form-data", ...headers };
       } else if (body instanceof Object) {
+        // 如果是对象，设置json格式
         _body = JSON.stringify(body);
         headers = { "Content-Type": "application/json", ...headers };
       } else {
@@ -89,16 +103,25 @@ export class Fetcher {
     };
   }
 
+  /**
+   * 原生fetch，可用于自定义请求，已加载拦截器和基础路径
+   */
   fetch: (
     input: string | URL,
     init?: RequestInit
   ) => Promise<Response | undefined>;
 
+  /**
+   * 发起get请求
+   */
   get: <Response = any, Query = Record<string, string | number>>(
     url: string,
     option?: { headers?: Record<string, string>; query?: Query }
   ) => Promise<Response>;
 
+  /**
+   * 发起post请求
+   */
   post: <Response = any, Body = any>(
     url: string,
     body?: Body,
